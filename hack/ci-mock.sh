ba7416527006d8e8bfa119a7462ad22e3314cbfa
matan <matan.wittner@gmail.com>
2020-03-17 14:52:16 +0200

    testing it

--- hack/ci-mock.sh
+++ hack/ci-mock.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-repository="stefanprodan/podinfo"
+repository="eu.gcr.io/boreal-byte-270613/echo"
 branch="master"
 version=""
 commit=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1 | awk '{print tolower($0)}')
 
-while getopts :r:b:v: o; do
-    case "${o}" in
-        r)
-            repository=${OPTARG}
-            ;;
-        b)
-            branch=${OPTARG}
-            ;;
-        v)
-            version=${OPTARG}
-            ;;
-    esac
-done
-shift $((OPTIND-1))
+# while getopts :r:b:v: o; do
+#     case "${o}" in
+#         r)
+#             repository=${OPTARG}
+#             ;;
+#         b)
+#             branch=${OPTARG}
+#             ;;
+#         v)
+#             version=${OPTARG}
+#             ;;
+#     esac
+# done
+# shift $((OPTIND-1))
 
 if [ -z "${version}" ]; then
     image="${repository}:${branch}-${commit}"
@@ -29,6 +29,6 @@ fi
 
 echo ">>>> Building image ${image} <<<<"
 
-docker build --build-arg GITCOMMIT=${commit} --build-arg VERSION=${version} -t ${image} -f Dockerfile.ci .
+docker build --build-arg GITCOMMIT=${commit} --build-arg VERSION=${version} -t ${image} -f ./echo/Dockerfile .
 
 docker push ${image}
